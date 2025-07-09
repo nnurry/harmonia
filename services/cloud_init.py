fbaabf2ea764d1fc1dac92579e0cab9b80242c37
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-07-10 00:36:19 +0700

    Clean services/cloud_init.py

--- services/cloud_init.py
+++ services/cloud_init.py
@@ -4,21 +4,21 @@ from config.cloud_init import CloudInitConfig
 from datetime import datetime, timezone
 
 
+def generate_ci_base_dir(ci_root_dir: str, prefix_name: str):
+    ts_version = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
+    return os.path.abspath((ci_root_dir + prefix_name + "/" + ts_version))
+
+
 def write_cloud_init_iso_to_disk(
     config: CloudInitConfig,
-    ci_root_dir: str,
+    ci_base_dir: str,
     iso_filename: str = "cidata.json",
 ):
-    ts_version = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
-    base_dir = os.path.abspath(
-        (ci_root_dir + config.user_data.hostname + "/" + ts_version)
-    )
+    os.makedirs(ci_base_dir, exist_ok=True)
 
-    os.makedirs(base_dir, exist_ok=True)
-
-    user_data_path = os.path.join(base_dir, "user-data")
-    meta_data_path = os.path.join(base_dir, "meta-data")
-    network_config_path = os.path.join(base_dir, "network-config")
+    user_data_path = os.path.join(ci_base_dir, "user-data")
+    meta_data_path = os.path.join(ci_base_dir, "meta-data")
+    network_config_path = os.path.join(ci_base_dir, "network-config")
 
     with (
         open(user_data_path, "w") as user_data_file,
@@ -29,10 +29,11 @@ def write_cloud_init_iso_to_disk(
         meta_data_file.write(config.meta_data.to_ci_format())
         network_config_file.write(config.network_config.to_ci_format())
 
+    iso_path = os.path.join(ci_base_dir, iso_filename)
     cmd_parts = [
         "mkisofs",
         "-output",
-        os.path.join(base_dir, iso_filename),
+        iso_path,
         "-volid",
         "cidata",
         "-joliet",
@@ -43,3 +44,4 @@ def write_cloud_init_iso_to_disk(
     ]
 
     subprocess.run(cmd_parts, check=True, capture_output=True, text=True, shell=False)
+    return iso_path
