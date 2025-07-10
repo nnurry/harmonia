31705e4c2969e13d138759bcfda3572449b170a7
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-07-11 00:38:30 +0700

    Make cloud-init service into class

--- services/cloud_init.py
+++ services/cloud_init.py
@@ -1,43 +1,46 @@
 import os
-from config.cloud_init import CloudInitConfig
 from datetime import datetime, timezone
 
+from config.cloud_init import CloudInitConfig
 from services.cli import LibvirtCLI
 
 
-def generate_ci_base_dir(ci_root_dir: str, prefix_name: str):
-    ts_version = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
-    return os.path.abspath((ci_root_dir + prefix_name + "/" + ts_version))
+class CloudInitService:
+    @staticmethod
+    def generate_ci_base_dir(ci_root_dir: str, prefix_name: str) -> str:
+        ts_version = datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")
+        return os.path.abspath(os.path.join(ci_root_dir, prefix_name, ts_version))
 
+    @staticmethod
+    def write_cloud_init_iso_to_disk(
+        config: CloudInitConfig,
+        ci_base_dir: str,
+        iso_filename: str = "cidata.iso",
+    ) -> str:
+        os.makedirs(ci_base_dir, exist_ok=True)
 
-def write_cloud_init_iso_to_disk(
-    config: CloudInitConfig,
-    ci_base_dir: str,
-    iso_filename: str = "cidata.json",
-):
-    os.makedirs(ci_base_dir, exist_ok=True)
+        user_data_path = os.path.join(ci_base_dir, "user-data")
+        meta_data_path = os.path.join(ci_base_dir, "meta-data")
+        network_config_path = os.path.join(ci_base_dir, "network-config")
+        iso_path = os.path.join(ci_base_dir, iso_filename)
 
-    user_data_path = os.path.join(ci_base_dir, "user-data")
-    meta_data_path = os.path.join(ci_base_dir, "meta-data")
-    network_config_path = os.path.join(ci_base_dir, "network-config")
+        with (
+            open(user_data_path, "w") as user_data_file,
+            open(meta_data_path, "w") as meta_data_file,
+            open(network_config_path, "w") as network_config_file,
+        ):
+            user_data_file.write(config.user_data.to_ci_format())
+            meta_data_file.write(config.meta_data.to_ci_format())
+            network_config_file.write(config.network_config.to_ci_format())
 
-    with (
-        open(user_data_path, "w") as user_data_file,
-        open(meta_data_path, "w") as meta_data_file,
-        open(network_config_path, "w") as network_config_file,
-    ):
-        user_data_file.write(config.user_data.to_ci_format())
-        meta_data_file.write(config.meta_data.to_ci_format())
-        network_config_file.write(config.network_config.to_ci_format())
+        LibvirtCLI.create_cloud_init_iso(
+            iso_path,
+            [
+                user_data_path,
+                meta_data_path,
+                network_config_path,
+            ],
+        )
 
-    iso_path = os.path.join(ci_base_dir, iso_filename)
-    LibvirtCLI.create_cloud_init_iso(
-        iso_path,
-        [
-            user_data_path,
-            meta_data_path,
-            network_config_path,
-        ],
-    )
-
-    return iso_path
+        iso_path = os.path.join(ci_base_dir, iso_filename)
+        return iso_path
