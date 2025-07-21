38c536a91d3f692af6530108125dc585eda5ef78
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-07-21 16:37:45 +0000

    Add bare minimum constructor for CI service

--- internal/homevirt/service/cloudinit.go
+++ internal/homevirt/service/cloudinit.go
@@ -3,3 +3,10 @@ package service
 const (
 	DEFAULT_CLOUD_INIT_ISO_BASE_PATH = "/var/lib/libvirt/images"
 )
+
+type CloudInit struct {
+}
+
+func NewCloudInit() (*CloudInit, error) {
+	return &CloudInit{}, nil
+}
