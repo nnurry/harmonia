d9a7941a8ea90aba676756ab885c413ec8ff8c3f
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-07-25 14:57:50 +0000

    Simplify libvirt connect subcommand

--- cmd/homevirt/libvirt.go
+++ cmd/homevirt/libvirt.go
@@ -3,6 +3,7 @@ package homevirt
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/nnurry/harmonia/internal/homevirt/builder"
 	"github.com/nnurry/harmonia/pkg/types"
@@ -19,12 +20,6 @@ const (
 )
 
 type HomevirtCommand struct {
-	hypervisor    string
-	transportType string
-	user          string
-	host          string
-	keyfilePath   string
-
 	connectUrl string
 }
 
@@ -39,32 +34,6 @@ func (command *HomevirtCommand) Signature() string {
 func (command *HomevirtCommand) Flags() []cli.Flag {
 	return []cli.Flag{
 		&cli.StringFlag{
-			Name:        "hypervisor",
-			DefaultText: builder.LIBVIRT_CONNECT_URL_DEFAULT_HYPERVISOR,
-			Usage:       "Set type of hypervisor. Omit to use 'qemu' (no support for xen though, hehe)",
-			Destination: &command.hypervisor,
-		},
-		&cli.StringFlag{
-			Name:        "transport-type",
-			Usage:       "Use 'ssh' to connect remotely. I do not support other type of connections. If this is run on the same machine as hypervisor, omitting should be fine",
-			Destination: &command.transportType,
-		},
-		&cli.StringFlag{
-			Name:        "user",
-			Usage:       "Set system user. Omit to use default",
-			Destination: &command.user,
-		},
-		&cli.StringFlag{
-			Name:        "host",
-			Usage:       "Host address of hypervisor. Omit to use 'localhost' (only when hypervisor is on the same machine, otherwise recommend to fill the address)",
-			Destination: &command.host,
-		},
-		&cli.StringFlag{
-			Name:        "keyfile-path",
-			Usage:       "Path to SSH private key for 'ssh' transport type. Omit if SSH config already has matching identity",
-			Destination: &command.keyfilePath,
-		},
-		&cli.StringFlag{
 			Name:        "connect-url",
 			Usage:       "Set Homevirt connect URL. Using this flag would discard all other connect flags",
 			Destination: &command.connectUrl,
@@ -77,6 +46,8 @@ func (command *HomevirtCommand) Subcommands() []*cli.Command {
 		(&DefineLibvirtDomainCommand{}).Build(),
 		(&RemoveLibvirtDomainCommand{}).Build(),
 		(&ListLibvirtDomainsCommand{}).Build(),
+		(&StartLibvirtDomainCommand{}).Build(),
+		(&StopLibvirtDomainCommand{}).Build(),
 	}
 }
 
@@ -89,57 +60,17 @@ func (command *HomevirtCommand) Handler() func(ctx *cli.Context) error {
 func (command *HomevirtCommand) Build() *cli.Command {
 	cliCommand := utils.ConvertInternalCommandToCliCommand(command)
 	cliCommand.Before = func(ctx *cli.Context) error {
-		if command.connectUrl != "" {
-			connectBuilder, err := builder.NewLibvirtConnectBuilderFromConnectUrl(command.connectUrl)
-
-			if err != nil {
-				return err
-			}
-
-			ctx.Context = context.WithValue(ctx.Context, HOMEVIRT_COMMAND_CONNECT_BUILDER_CTX_KEY, connectBuilder)
-			return nil
-		}
-
-		connectBuilder, err := builder.NewLibvirtConnectBuilder(
-			[]*builder.ConnectUrlBuilderFlag{
-				builder.SET_TRANSPORT_CONF,
-				builder.SET_USER,
-				builder.SET_HOST,
-				builder.SET_KEYFILE,
-			},
-			false,
-		)
+		connectBuilder, err := builder.NewLibvirtConnectBuilderFromConnectUrl(command.connectUrl)
 
 		if err != nil {
 			return err
 		}
 
-		if command.hypervisor != "" {
-			connectBuilder = connectBuilder.WithHypervisor(command.hypervisor)
-		}
-
-		if command.transportType != "" {
-			connectBuilder = connectBuilder.WithTransportType(command.transportType)
-		}
-
-		if command.user != "" {
-			connectBuilder = connectBuilder.WithUser(command.user)
-		}
-
-		if command.host != "" {
-			connectBuilder = connectBuilder.WithHost(command.host)
-		}
-
-		if command.keyfilePath != "" {
-			connectBuilder = connectBuilder.WithKeyfilePath(command.keyfilePath)
-		}
-
-		if err = connectBuilder.Verify(); err != nil {
-			return err
-		}
+		log.Printf("Setting Libvirt connection with URL = %v\n", command.connectUrl)
 
 		ctx.Context = context.WithValue(ctx.Context, HOMEVIRT_COMMAND_CONNECT_BUILDER_CTX_KEY, connectBuilder)
 		return nil
+
 	}
 
 	return cliCommand
