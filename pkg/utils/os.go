fb1fcad981eaf8849bc4f30087cc09edc57e7469
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-07-27 09:33:53 +0000

    Add util to generate random timestamp-aware folder name (for cloud-init ISO)

---
+++ pkg/utils/os.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+func GenerateUniqueTimestampDirName() string {
+	stringBuffer := bytes.NewBufferString("")
+
+	now := time.Now()
+	fmt.Fprintf(stringBuffer, "/%v", now.Format("20060102150405"))
+	fmt.Fprintf(stringBuffer, "%03d", now.Nanosecond()/1_000_000)
+
+	return stringBuffer.String()
+}
