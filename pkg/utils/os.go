ebe5573fc7b87610cab694e39f9877966b2bf9d3
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2025-08-11 10:24:54 +0000

    Remove redundant slash (it looks stupid)

--- pkg/utils/os.go
+++ pkg/utils/os.go
@@ -10,7 +10,7 @@ func GenerateUniqueTimestamp() string {
 	stringBuffer := bytes.NewBufferString("")
 
 	now := time.Now()
-	fmt.Fprintf(stringBuffer, "/%v", now.Format("20060102150405"))
+	fmt.Fprintf(stringBuffer, "%v", now.Format("20060102150405"))
 	fmt.Fprintf(stringBuffer, "%03d", now.Nanosecond()/1_000_000)
 
 	return stringBuffer.String()
